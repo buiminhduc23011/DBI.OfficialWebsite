49ec42657f6207fcbb82c803ad4353ca11db945b
Ducne <49405614+buiminhduc23011@users.noreply.github.com>
2025-10-04 15:29:10 +0700

    remove APIkey

--- dbi-react-app/src/firebase.js
+++ dbi-react-app/src/firebase.js
@@ -2,19 +2,16 @@
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Using environment variables for security
 const firebaseConfig = {
-  apiKey: "AIzaSyA-EFFDmnW7nknLIO62oxmIBffrSiCEXII",
-  authDomain: "dbi-react-app.firebaseapp.com",
-  projectId: "dbi-react-app",
-  storageBucket: "dbi-react-app.firebasestorage.app",
-  messagingSenderId: "529009076143",
-  appId: "1:529009076143:web:cd9063f4f3406e0d69f15a",
-  measurementId: "G-3N6D95PVMX"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
